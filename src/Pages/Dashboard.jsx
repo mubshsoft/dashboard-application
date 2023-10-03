cd0f4d04039af03f035ef13633977de571d4a673
md-mubashir <52619518+md-mubashir@users.noreply.github.com>
2023-10-03 12:47:50 +0530

    update code for optimization

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
-import { RiArrowDownSFill, RiArrowUpSFill, RiTimeLine } from 'react-icons/ri';
+import { RiArrowDownSFill, RiTimeLine } from 'react-icons/ri';
 import { IoMdArrowForward } from 'react-icons/io';
-import { PiVideoCameraThin } from 'react-icons/pi'
-import Calendar from "../components/Calendar"
-import BarChartComponent from "../components/BarChartComponent.jsx"
+import { PiVideoCameraThin } from 'react-icons/pi';
 
-import { Space } from 'antd';
+import Calendar from '../components/Calendar';
+import BarChartComponent from '../components/BarChartComponent';
 import TransactionHistory from '../components/TransactionHistory';
-const onClick = ({ key }) => {
-  message.info(`Click on item ${key}`);
-};
-
 
 const Dashboard = () => {
+  const avatarCount = 3;
 
-  const avatarCount = 3; // Number of avatars
+  const generateAvatars = (count) => {
+    const avatars = [];
+    for (let index = 0; index < count; index++) {
+      avatars.push(
+        <img
+          key={index}
+          src="https://demos.creative-tim.com/notus-js/assets/img/team-1-800x800.jpg"
+          className={`w-10 h-10 rounded-full ${index > 0 ? '-ml-5' : ''}`}
+          alt={`Avatar ${index + 1}`}
+        />
+      );
+    }
+    return avatars;
+  };
 
-  const avatars = Array.from({ length: avatarCount }, (_, index) => (
-    <img
-      key={index}
-      src="https://www.pngkey.com/png/full/72-729716_user-avatar-png-graphic-free-download-icon.png"
-      className={`w-[40px] ${index > 0 ? '-ml-5' : ''}`}
-      alt={`Avatar ${index + 1}`}
-    />
-  ));
   return (
     <>
-      {/* bg-[#6e62e5] */}
-      <div className="md:flex m-4 gap-5">
-        {/* First Card (Column Span 10 on medium screens, Column Span 2 on small and large screens) */}
-        <div className="w-full md:w-full rounded-2xl lg:w-9/12 p-4 bg-[#6e62e5]">
+      {/* First Card */}
+      <div className="sm:flex">
+        {/* Left Column (Column Span 12 on small screens, 9 on medium screens, 10 on large screens) */}
+        <div className="w-full sm:w-9/12 lg:w-10/12 p-4 bg-[#6e62e5] rounded-2xl m-4">
           <div className="flex flex-wrap">
             {Array.from({ length: 3 }, (_, index) => (
-              <div key={index} className="w-full md:w-full lg:w-1/3 p-1">
+              <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-1">
                 <div className="p-2">
                   <div className="flex items-center justify-center gap-3">
                     <img
-                      src="https://www.pngkey.com/png/full/72-729716_user-avatar-png-graphic-free-download-icon.png"
-                      className="w-[50px]"
+                      src="https://t3.ftcdn.net/jpg/01/22/09/76/500_F_122097688_6zBkh5v1j8OLHU1SaFgQNvx1K7weNxQK.jpg"
+                      className="w-10 h-10 rounded-full"
                       alt="Avatar"
                     />
                     <div>
@@ -52,80 +53,66 @@ const Dashboard = () => {
           </div>
         </div>
 
-
-        {/* <!-- Second Card (Column Span 2 on medium screens, Column Span 2 on small and large screens) --> */}
-        <div className="w-full md:w-full lg:w-3/12 px-3">
+        {/* Right Column (Column Span 12 on small screens, 3 on medium screens and large screens) */}
+        <div className="w-full sm:w-3/12 p-4">
           <div className="bg-white">
             <h6 className="text-[#6e62e5]">Premium Access</h6>
-            <div className="lg:relative md:flex-col">
-              <h1 className="lg:text-3xl md:text-xl font-semibold custom font-custom">
-                Take Back<br /> Your Creative <br />Control
-              </h1>
-              <div className="flex flex-wrap -mx-2 lg:absolute lg:right-0 bottom-0 xl:right-20">
-                {avatars}
+            <h1 className="text-xl lg:text-3xl font-semibold custom font-custom">
+              Take Back<br /> Your Creative <br />Control
+            </h1>
+            <div className='relative'>
+                <div className="flex absolute left-28 -bottom-3 md:right-28 md:-bottom-3 lg:right-1 lg:bottom-1"> 
+                {generateAvatars(avatarCount)}
+                </div>
               </div>
-            </div>
           </div>
         </div>
-
       </div>
 
-      {/* second div */}
-      <div className="md:flex">
-        {/* <!-- First Card (Column Span 10 on medium screens, Column Span 2 on small and large screens) --> */}
-        <div className="w-full md:w-9/12 lg:w-9/12 px-4">
-          <div className="bg-white rounded-md  p-4 border border-gary-200">
+      {/* Second Card */}
+      <div className="sm:flex">
+        {/* Left Column (Column Span 12 on small screens, 9 on medium screens, 10 on large screens) */}
+        <div className="w-full sm:w-9/12 lg:w-10/12 p-4">
+          <div className="bg-white rounded-md p-4 border border-gray-200">
             <div className="flex flex-wrap">
-              {/* <!-- First Card (Full width on small screens, 1/2 width on medium screens, 1/3 width on large screens) --> */}
-              <div className="w-full md:w-1/2 lg:w-1/2">
-                <div className="flex flex-col justify-between h-[150px]">
+              {/* First Card (Full width on small screens, 1/2 width on medium screens, 1/3 width on large screens) */}
+              <div className="w-full sm:w-1/2 lg:w-1/2">
+                <div className="flex flex-col gap-20">
                   <div>
                     <p className="text-black font-semibold text-lg -mb-1">Top Sales & Cost</p>
                     <span className="text-gray-400 text-xs">Last 60 days</span>
                   </div>
-
                   <div className="flex items-center gap-3">
                     <h2 className="text-[#6e62e5] font-bold text-4xl">$956.82K</h2>
-                    <div className="bg-green-200 rounded-3xl px-4 py-1 w-[100px] h-[25px] flex items-center text-green-500 font-semibold font-custom">
-                      <RiArrowUpSFill />+5.4%
+                    <div className="bg-green-200 rounded-3xl px-4 py-1 w-32 h-10 flex items-center text-green-500 font-semibold font-custom">
+                      <RiArrowDownSFill />+5.4%
                     </div>
                   </div>
                 </div>
-
               </div>
 
-              {/* <!-- Second Card (Full width on small screens, 1/2 width on medium screens, 1/3 width on large screens) --> */}
-              <div className="w-full md:w-1/2 lg:w-1/2">
+              {/* Second Card (Full width on small screens, 1/2 width on medium screens, 1/3 width on large screens) */}
+              <div className="w-full sm:w-1/2 lg:w-1/2">
                 <div className="rounded-md">
                   <div className="flex items-center justify-between px-10">
-                    <div className='text-black font-semibold font-custom text-sm'>Analytic<span className='text-green-400 font-semibold text-sm ml-2'>+5.4%</span></div>
-
-                    <div className='text-black font-semibold font-custom flex items-center cursor-pointer text-xs'>Month <RiArrowDownSFill /></div>
-
+                    <div className="text-black font-semibold font-custom text-sm">Analytic <span className="text-green-400 font-semibold text-sm ml-2">+5.4%</span></div>
+                    <div className="text-black font-semibold font-custom flex items-center cursor-pointer text-xs">Month <RiArrowDownSFill /></div>
                   </div>
                   <BarChartComponent />
                 </div>
               </div>
-
-              {/* <!-- Third Card (Full width on small screens, 1/2 width on medium screens, 1/3 width on large screens) --> */}
-
             </div>
           </div>
         </div>
 
-        {/* <!-- Second Card (Column Span 2 on medium screens, Column Span 2 on small and large screens) --> */}
-        <div className="w-full md:w-3/12 lg:w-3/12 px-4">
+        {/* Right Column (Column Span 12 on small screens, 3 on medium screens and large screens) */}
+        <div className="w-full sm:w-3/12 p-4">
           <div className="bg-white rounded-md p-4">
-            <div>
-              <div className='text-gray-500 font-semibold  flex items-center cursor-pointer text-sm'>The Professional Platform <RiArrowDownSFill /></div>
-              <div className='flex justify-between text-[#6e62e5] mt-5 text-lg border border-gray-300 py-1 px-4 rounded-xl cursor-pointer'>
-                <button>
-                  Upgrade Now
-                </button>
-                <IoMdArrowForward className='text-2xl ' />
-              </div>
+            <div className="text-gray-500 font-semibold flex items-center cursor-pointer text-sm">The Professional Platform <RiArrowDownSFill /></div>
+            <div className="flex justify-between text-[#6e62e5] mt-5 text-lg border border-gray-300 py-1 px-4 rounded-xl cursor-pointer">
+              <button>Upgrade Now</button>
+              <IoMdArrowForward className="text-2xl" />
             </div>
-
             <div className="flex justify-center items-center mt-5">
               <Calendar />
             </div>
@@ -133,62 +120,58 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className='bg-red-400'>
-        {/* <ResponsiveTable /> */}
-      </div>
+      <div className="bg-red-400">{/* <ResponsiveTable /> */}</div>
 
-      <div className="md:flex">
-        {/* <!-- First Card (Column Span 10 on medium screens, Column Span 2 on small and large screens) --> */}
-        <div className="w-full md:w-9/12 lg:w-9/12 p-4">
+      <div className="sm:flex">
+        {/* Left Column (Column Span 12 on small screens, 9 on medium screens, 10 on large screens) */}
+        <div className="w-full sm:w-9/12 lg:w-10/12 p-4">
           <TransactionHistory />
         </div>
-        <div className='w-full md:w-full lg:w-3/12 px-3 mt-10'>
-          <div className='flex gap-4 items-center'>
-            <div><img src="https://demos.creative-tim.com/notus-js/assets/img/team-1-800x800.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" /></div>
+
+        {/* Right Column (Column Span 12 on small screens, 3 on medium screens and large screens) */}
+        <div className="w-full sm:w-3/12 p-4 mt-5">
+          <div className="flex gap-4 items-center">
             <div>
-              <p className='font-semibold text-sm mb-1'> Meeting with Client</p>
-              <div className='flex justify-between items-center gap-10 text-xs text-gray-500'>
-                <p className='flex items-center gap-1'><PiVideoCameraThin /> meet</p>
-                <p className='flex items-center gap-1'><RiTimeLine /> 12Pm</p>
+              <img src="https://demos.creative-tim.com/notus-js/assets/img/team-1-800x800.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" />
+            </div>
+            <div>
+              <p className="font-semibold text-sm mb-1">Meeting with Client</p>
+              <div className="flex justify-between items-center gap-4 text-xs text-gray-500">
+                <p className="flex items-center gap-1"><PiVideoCameraThin /> Google meet</p>
+                <p className="flex items-center gap-1"><RiTimeLine /> 12 PM</p>
               </div>
             </div>
           </div>
-          <div className='border-b border-gray-300 mt-5'>
-          </div>
-          <div className='flex gap-4 items-center mt-5'>
-            <div><img src="https://img.freepik.com/premium-photo/3d-render-stack-brightly-colored-books-with-bookmark-pile-textbooks-notebooks-books-symbol-learning-school-business-isolated-illustration-gray-background_172597-1789.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" /></div>
+          <div className="border-b border-gray-300 mt-5"></div>
+          <div className="flex gap-4 items-center mt-5">
             <div>
-              <p className='font-semibold text-sm mb-1'> Weekly Report</p>
-              <div className='flex justify-between items-center gap-10 text-xs text-gray-500'>
-                <p className='flex items-center gap-1'><PiVideoCameraThin /> meet</p>
-                <p className='flex items-center gap-1'><RiTimeLine /> 03Pm</p>
+              <img src="https://img.freepik.com/premium-photo/3d-render-stack-brightly-colored-books-with-bookmark-pile-textbooks-notebooks-books-symbol-learning-school-business-isolated-illustration-gray-background_172597-1789.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" />
+            </div>
+            <div>
+              <p className="font-semibold text-sm mb-1">Weekly Report</p>
+              <div className="flex justify-between items-center gap-4 text-xs text-gray-500">
+                <p className="flex items-center gap-1"><PiVideoCameraThin /> Google meet</p>
+                <p className="flex items-center gap-1"><RiTimeLine /> 03 PM</p>
               </div>
             </div>
           </div>
-          <div className='border-b border-gray-300 mt-5'>
-          </div>
-          <div className='flex gap-4 items-center mt-5'>
-            <div><img src="https://media.istockphoto.com/id/1128342917/photo/mock-up-magazines-book-or-catalog-on-white-background.jpg?s=612x612&w=0&k=20&c=eRcNkG6718RwXcBIk6EXKDVjve61jQeHLwuvuKQm0zw=" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" /></div>
+          <div className="border-b border-gray-300 mt-5"></div>
+          <div className="flex gap-4 items-center mt-5">
             <div>
-              <p className='font-semibold text-sm mb-1'> Daily Scrum Meeting</p>
-              <div className='flex justify-between items-center gap-10 text-xs text-gray-500'>
-                <p className='flex items-center gap-1'><PiVideoCameraThin /> meet</p>
-                <p className='flex items-center gap-1'><RiTimeLine /> 05Pm</p>
+              <img src="https://img.freepik.com/premium-photo/3d-render-stack-brightly-colored-books-with-bookmark-pile-textbooks-notebooks-books-symbol-learning-school-business-isolated-illustration-gray-background_172597-1789.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" />
+            </div>
+            <div>
+              <p className="font-semibold text-sm mb-1">Daily Scrum Meeting</p>
+              <div className="flex justify-between items-center gap-4 text-xs text-gray-500">
+                <p className="flex items-center gap-1"><PiVideoCameraThin /> Google meet</p>
+                <p className="flex items-center gap-1"><RiTimeLine /> 05 PM</p>
               </div>
             </div>
-          </div>
-          <div className='border-b border-gray-300 mt-5'>
           </div>
         </div>
-
       </div>
-
-
-
-
-
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -137,67 +137,7 @@ const TransactionHistory = () => {
                 </tbody>
               </table>
               {/* <!-- Edit user modal --> */}
-              <div id="editUserModal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md-inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative w-full max-w-2xl max-h-full">
-                  {/* <!-- Modal content --> */}
-                  <form action="#" className="relative bg-white rounded-lg shadow dark-bg-gray-700">
-                    {/* <!-- Modal header --> */}
-                    <div className="flex items-start justify-between p-4 border-b rounded-t dark-border-gray-600">
-                      <h3 className="text-xl font-semibold text-gray-900 dark-text-white">
-                        Edit user
-                      </h3>
-                      <button type="button" className="text-gray-400 bg-transparent hover-bg-gray-200 hover-text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark-hover-bg-gray-600 dark-hover-text-white" data-modal-hide="editUserModal">
-                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg>
-                        <span className="sr-only">Close modal</span>
-                      </button>
-                    </div>
-                    {/* <!-- Modal body --> */}
-                    <div className="p-6 space-y-6">
-                      <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="first-name" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">First Name</label>
-                          <input type="text" name="first-name" id="first-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="Bonnie" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="last-name" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">Last Name</label>
-                          <input type="text" name="last-name" id="last-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="Green" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">Email</label>
-                          <input type="email" name="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="example@company.com" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="phone-number" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">Phone Number</label>
-                          <input type="number" name="phone-number" id="phone-number" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="e.g. +(12)3456 789" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="department" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">Department</label>
-                          <input type="text" name="department" id="department" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="Development" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">Company</label>
-                          <input type="number" name="company" id="company" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="123456" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="current-password" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">Current Password</label>
-                          <input type="password" name="current-password" id="current-password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="••••••••" required="" />
-                        </div>
-                        <div className="col-span-6 sm-col-span-3">
-                          <label htmlFor="new-password" className="block mb-2 text-sm font-medium text-gray-900 dark-text-white">New Password</label>
-                          <input type="password" name="new-password" id="new-password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-blue-600 focus-border-blue-600 block w-full p-2.5 dark-bg-gray-600 dark-border-gray-500 dark-placeholder-gray-400 dark-text-white dark-focus-ring-blue-500 dark-focus-border-blue-500" placeholder="••••••••" required="" />
-                        </div>
-                      </div>
-                    </div>
-                    {/* <!-- Modal footer --> */}
-                    <div className="flex justify-end p-4 border-t border-gray-300 rounded-b dark-border-gray-600">
-                      <button type="button" className="text-gray-600 dark-text-gray-400 bg-gray-100 hover-bg-gray-200 dark-hover-bg-gray-600 hover-text-gray-900 dark-hover-text-white border border-gray-300 dark-border-gray-600 rounded-lg text-sm px-6 py-2.5 w-1/5 mr-2" data-modal-hide="editUserModal">Cancel</button>
-                      <button type="submit" className="bg-blue-600 hover-bg-blue-700 text-white dark-bg-blue-500 dark-hover-bg-blue-600 font-medium rounded-lg text-sm px-6 py-2.5 w-1/5" data-modal-hide="editUserModal">Save</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
+              
             </div>
 
           </div>
